81508107588241b5e9c755fe74a2656908b8593b
陈金浩 <171111754@qq.com>
2019-02-26 17:20:52 +0800

    development修改

--- configs/webpack.development.js
+++ configs/webpack.development.js
@@ -48,7 +48,7 @@ module.exports = merge(baseConfig, {
 	},
 	plugins: [
 		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': "development",
+			'process.env.NODE_ENV': '"development"',
 		}),
 		new webpack.HotModuleReplacementPlugin()
 	]

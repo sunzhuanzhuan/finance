4a430fe6c2a99175e1bb0f7f09c28bd9e288cc6a
陈金浩 <171111754@qq.com>
2019-04-10 15:45:57 +0800

    修改api

--- src/financeSetting/constants/Interface.js
+++ src/financeSetting/constants/Interface.js
@@ -1,6 +1,6 @@
 export default {
-	postTrinityProfitRateAll: '/price/finance/paramsSetting/trinityProfitRate/v1/getAll',
-	getTrinityCompanyList: '/platform/v1/queryPlatformList',
-	postTrinityProfitRateAdd: '/price/finance/paramsSetting/trinityProfitRate/v1/add',
-	postTrinityProfitRateModify: '/price/finance/paramsSetting/trinityProfitRate/v1/modify',
+	postTrinityProfitRateAll: '/common-price/finance/paramsSetting/trinityProfitRate/v1/getAll',
+	getTrinityCompanyList: '/operator-gateway/common/v1/platformList',
+	postTrinityProfitRateAdd: '/common-price/finance/paramsSetting/trinityProfitRate/v1/add',
+	postTrinityProfitRateModify: '/common-price/finance/paramsSetting/trinityProfitRate/v1/modify',
 }

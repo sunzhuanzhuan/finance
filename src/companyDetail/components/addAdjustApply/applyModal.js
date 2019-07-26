b1d61e60a61128171c8df1a6b416d89b6b89f7d8
baikeqi <baikeqii@163.com>
2019-07-26 15:15:45 +0800

    订单调价 金额调价 正则修改

--- src/companyDetail/components/addAdjustApply/applyModal.js
+++ src/companyDetail/components/addAdjustApply/applyModal.js
@@ -350,7 +350,7 @@ class ApplyModal extends React.Component {
 	}
 
 	checkCountNum = (_, value, callback) => {
-		const reg = /^(?!(0[0-9]{0,}$))[0-9]{1,}[.]{0,}[0-9]{0,}$/;
+		const reg = /^([1-9]\d*(\.\d*[1-9])?)|(0\.\d*[1-9])$/;
 		if (value) {
 			if (reg.test(value.toString())) {
 				callback();

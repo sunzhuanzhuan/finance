b1bb1942f6d1d42866a896da27fe9a80fb0b4a20
baikeqi <baikeqii@163.com>
2019-08-02 12:47:39 +0800

    订单调价 添加申请页面首次渲染不请求列表数据

--- src/companyDetail/containers/addAdjustApply.js
+++ src/companyDetail/containers/addAdjustApply.js
@@ -34,15 +34,15 @@ class AddAdjustApply extends React.Component {
 		this.props.actions.getCompanyDetailAuthorizations();
 		delete search['requirement_label'];
 		const { keys:{company_id} } = search;
-		const queryObj = { page: 1, ...search.keys, page_size: 200 };
+		// const queryObj = { page: 1, ...search.keys, page_size: 200 };
 		if(company_id) {
 			const companyId = company_id.key;
-			delete search.keys.company_id;
-			Object.assign(queryObj, {company_id: companyId});
+			// delete search.keys.company_id;
+			// Object.assign(queryObj, {company_id: companyId});
 			this.setState({companyId});
 		}
 
-		this.queryData(queryObj);
+		// this.queryData(queryObj);
 	}
 	queryData = (obj, func) => {
 		this.setState({ loading: true });

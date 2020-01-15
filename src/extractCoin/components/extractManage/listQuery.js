cc5197a9d14760b932b0a9d8b4a7b6bddd381046
baikeqi <baikeqii@163.com>
2020-01-15 18:47:04 +0800

    快易提提测

--- src/extractCoin/components/extractManage/listQuery.js
+++ src/extractCoin/components/extractManage/listQuery.js
@@ -40,55 +40,41 @@ class ExtractQuery extends React.Component {
 	}
 	render() {
 		let { getFieldDecorator } = this.props.form;
-		const formItemLayout = {
-			labelCol: { span: 7 },
-			wrapperCol: { span: 14 },
-		};
 		const strConfig = {
 			rules: [{ type: 'string', required: false }]
 		}
 		return <div>
 			<Form className='extractManage-search-form'>
-				<Row>
-					<Col span={5}>
-						<FormItem label='提现单号' {...formItemLayout}>
-							{getFieldDecorator('withdraw_id', strConfig)(
-								<Input placeholder="请输入" />
-							)}
-						</FormItem>
-					</Col>
-					<Col span={5}>
-						<FormItem label='订单ID' {...formItemLayout}>
-							{getFieldDecorator('order_id', strConfig)(
-								<Input placeholder="请输入" />
-							)}
-						</FormItem>
-					</Col>
-					<Col span={5}>
-						<FormItem label='主账号名：' {...formItemLayout}>
-							{getFieldDecorator('identity_name', strConfig)(
-								<Input placeholder="请输入" />
-							)}
-						</FormItem>
-					</Col>
-					<Col span={5}>
-						<FormItem label='状态：' {...formItemLayout}>
-							{getFieldDecorator('status', { initialValue: '' })(
-								<Select style={{ width: 120 }}>
-									<Option value="">不限</Option>
-									<Option value="1">待审核</Option>
-									<Option value="2">待打款</Option>
-									<Option value="3">已打款</Option>
-									<Option value="4">审核失败</Option>
-								</Select>
-							)}
-						</FormItem>
-					</Col>
-					<Col span={4}>
-						<Button type="primary" htmlType="submit" onClick={this.handleSearch}>查询</Button>
-						<a className="reset-filter left-gap" onClick={this.handleClear}><Icon type="retweet" />重置搜索框</a>
-					</Col>
-				</Row>
+				<FormItem label='提现单号'>
+					{getFieldDecorator('withdraw_id', strConfig)(
+						<Input placeholder="请输入提现单，多个以空格分隔" style={{ width: 196 }} />
+					)}
+				</FormItem>
+				<FormItem label='订单ID'>
+					{getFieldDecorator('order_id', strConfig)(
+						<Input placeholder="请输入订单，多个以空格分隔" style={{ width: 196 }} />
+					)}
+				</FormItem>
+				<FormItem label='主账号名'>
+					{getFieldDecorator('identity_name', strConfig)(
+						<Input placeholder="请输入" />
+					)}
+				</FormItem>
+				<FormItem label='状态'>
+					{getFieldDecorator('status', { initialValue: '' })(
+						<Select style={{ width: 120 }}>
+							<Option value="">不限</Option>
+							<Option value="1">待审核</Option>
+							<Option value="2">待打款</Option>
+							<Option value="3">已打款</Option>
+							<Option value="4">审核失败</Option>
+						</Select>
+					)}
+				</FormItem>
+				<FormItem>
+					<Button type="primary" htmlType="submit" onClick={this.handleSearch}>查询</Button>
+					<a className="reset-filter left-gap" onClick={this.handleClear}><Icon type="retweet" />重置搜索框</a>
+				</FormItem>	
 			</Form>
 		</div>
 	}

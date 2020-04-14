8e73442a5001619b634a154b7c7693557ce64da7
baikeqi <baikeqii@163.com>
2020-04-14 15:58:59 +0800

    财务参数设置工作室平均回票税率名称修改

--- src/financeParamsSetting/constants/index.js
+++ src/financeParamsSetting/constants/index.js
@@ -3,7 +3,7 @@ import { Statistic } from 'antd'
 import { accMul } from '@/util'
 export const financeParams = [
 	{ label: '工作室平均服务费率', key: 'work_room_service_rate', isPercent: true },
-	{ label: '工作室平均回票税率', key: 'work_room_average_return_rate', isPercent: true },
+	{ label: '工作室平均发票税率', key: 'work_room_average_return_rate', isPercent: true },
 	{ label: '微播易销项税税率', key: 'wby_output_tax_rate', isPercent: true },
 	{ label: '布谷鸟销项税税率', key: 'bgn_output_tax_rate', isPercent: true },
 	{ label: '周打款限额', key: 'week_pay_ceiling' },
